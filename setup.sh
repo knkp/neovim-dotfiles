191a2636a00df6226d5935bb64e7cdc9b4300bce
knkp <knkp934@gmail.com>
2025-05-12 15:04:14 -0400

    More fixes

--- setup.sh
+++ setup.sh
@@ -24,14 +24,18 @@ if [ -f /etc/os-release ]; then
     case "$ID" in
         ubuntu|debian)
             sudo apt update
-            sudo apt install -y neovim python3 python3-pip python3-venv pipx git ripgrep fzf imagemagick curl
-            # Lazygit via PPA
-            sudo add-apt-repository ppa:lazygit-team/release -y
-            sudo apt update
-            sudo apt install -y lazygit
-            # Node.js via NodeSource
+            sudo apt install -y curl git ripgrep fzf imagemagick python3 python3-pip python3-venv pipx
+
+            # Install Lazygit manually
+            LAZYGIT_VERSION=$(curl -s https://api.github.com/repos/jesseduffield/lazygit/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+            curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION#v}_Linux_x86_64.tar.gz"
+            tar xf lazygit.tar.gz
+            sudo mv lazygit /usr/local/bin
+            rm lazygit.tar.gz
+
+            # Install Node.js and npm
             curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-            sudo apt install -y nodejs
+            sudo apt install -y nodejs npm
             ;;
         arch)
             sudo pacman -Syu --noconfirm neovim python python-pip nodejs npm git ripgrep fzf imagemagick lazygit
@@ -54,7 +58,7 @@ fi
 
 # Install Python support for Neovim
 echo "Installing Python support for Neovim..."
-pipx install pynvim
+pip3 install --user pynvim --break-system-packages
 
 # Install Node.js support for Neovim
 echo "Installing Node.js support for Neovim..."
@@ -68,3 +72,4 @@ if command -v nvim &> /dev/null; then
 fi
 
 echo "Setup complete! You can now start Neovim."
+
